06ece1443f5f4e7c99b05a40c2d0f624892024ac
Izzat Alharis <izzatalharist@gmail.com>
2026-07-10 13:54:36 +0700

    Save a new React app

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles/Main.sass';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Permata Komputer</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
